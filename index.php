fc9202f341abb06014031ed5dedd743360d201e8
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2017-12-25 13:42:10 +0800

    1219

--- index.php
+++ index.php
@@ -38,26 +38,29 @@
 
 <main role="main" class="main-sp">
   <!--commercial-->
-    <?php
-    $args = array(
-        'category_name' => 'commercial');
-    $posts = get_posts($args);
-    ?>
-    <?php if ($posts) : ?>
-        <?php foreach ($posts as $post) :
-            setup_postdata($post); ?>
-        <a class="swiper-slide" href="<?php the_permalink(); ?>">
-            <?php the_post_thumbnail(); ?>
-        </a>
-        <?php endforeach; ?>
-    <?php endif; ?>
+  <section class="commercial-con">
+      <?php
+      $args = array(
+          'category_name' => 'commercial');
+      $posts = get_posts($args);
+      ?>
+      <?php if ($posts) : ?>
+          <?php foreach ($posts as $post) :
+              setup_postdata($post); ?>
+          <a href="<?php the_permalink(); ?>">
+              <?php the_post_thumbnail(); ?>
+          </a>
+          <?php endforeach; ?>
+      <?php endif; ?>
+  </section>
+
   <!--brand-->
   <section class="category-section">
     <header>品<br>牌<br>brand</header>
       <?php
       $args = array(
           'category_name' => 'brand',
-          '');
+          'showposts' => '6');
       $posts = get_posts($args);
       get_template_part('loop');
       ?>
@@ -85,5 +88,84 @@
     </section>
   </section>
 
+  <!--pack-->
+  <section class="category-section">
+    <header>包<br>装<br>pack</header>
+      <?php
+      $args = array(
+          'category_name' => 'brand',
+          'showposts' => '6');
+      $posts = get_posts($args);
+      get_template_part('loop');
+      ?>
+    <section class="container no-padding swiper-con">
+      <div class="swiper-container">
+        <div class="swiper-wrapper">
+
+            <?php
+            $args = array(
+                'category_name' => 'swiper-banner-2'
+            );
+            $posts = get_posts($args);
+            ?>
+            <?php if ($posts) : ?>
+                <?php foreach ($posts as $post) :
+                    setup_postdata($post); ?>
+                <a class="swiper-slide" href="<?php the_permalink(); ?>">
+                    <?php the_post_thumbnail(); ?>
+                </a>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </div>
+        <div class="swiper-pagination"></div>
+      </div>
+    </section>
+  </section>
+
+  <!--sign-->
+  <section class="category-section">
+    <header>标<br>志<br>sign</header>
+      <?php
+      $args = array(
+          'category_name' => 'brand',
+          'showposts' => '6');
+      $posts = get_posts($args);
+      get_template_part('loop');
+      ?>
+    <section class="container no-padding swiper-con">
+      <div class="swiper-container">
+        <div class="swiper-wrapper">
+
+            <?php
+            $args = array(
+                'category_name' => 'swiper-banner-3'
+            );
+            $posts = get_posts($args);
+            ?>
+            <?php if ($posts) : ?>
+                <?php foreach ($posts as $post) :
+                    setup_postdata($post); ?>
+                <a class="swiper-slide" href="<?php the_permalink(); ?>">
+                    <?php the_post_thumbnail(); ?>
+                </a>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </div>
+        <div class="swiper-pagination"></div>
+      </div>
+    </section>
+  </section>
+
+  <!--inset-->
+  <section class="category-section">
+    <header>插<br>画<br>inset</header>
+      <?php
+      $args = array(
+          'category_name' => 'brand',
+          'showposts' => '6');
+      $posts = get_posts($args);
+      get_template_part('loop');
+      ?>
+  </section>
 </main>
 <?php get_footer(); ?>
